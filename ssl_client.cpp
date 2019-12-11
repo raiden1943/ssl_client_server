1b0fb9583d18a01d51a2e520c79a17fe8e129057
raiden1943 <20745e@gmail.com>
2019-12-11 23:07:20 +0900

    Edit pthread function

--- ssl_client.cpp
+++ ssl_client.cpp
@@ -8,7 +8,30 @@
 #include <netdb.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <pthread.h>
 #define FAIL    -1
+
+const int BUFSIZE = 1024;
+
+void *t_func(void *data)
+{
+	SSL *ssl = *((SSL **)data);
+	
+	while (1)
+	{
+		char buf[BUFSIZE];
+        int received = SSL_read(ssl, buf, sizeof(buf)); /* get reply & decrypt */
+        if (received <= 0) 
+        {
+        	SSL_get_error(ssl, received);
+        	break;
+		}
+        buf[received] = 0;
+        printf("%s\n", buf);
+	}
+	exit(0);
+}
+
 int OpenConnection(const char *hostname, int port)
 {
     int sd;
@@ -34,11 +57,11 @@ int OpenConnection(const char *hostname, int port)
 }
 SSL_CTX* InitCTX(void)
 {
-    //SSL_METHOD *method;
+    const SSL_METHOD *method;
     SSL_CTX *ctx;
     OpenSSL_add_all_algorithms();  /* Load cryptos, et.al. */
     SSL_load_error_strings();   /* Bring in and register error messages */
-    const SSL_METHOD *method = TLSv1_2_client_method();  /* Create new client-method instance */
+    method = DTLS_client_method();  /* Create new client-method instance */
     ctx = SSL_CTX_new(method);   /* Create new context */
     if ( ctx == NULL )
     {
@@ -69,15 +92,13 @@ void ShowCerts(SSL* ssl)
 int main(int count, char *strings[])
 {
     SSL_CTX *ctx;
-    int server;
     SSL *ssl;
-    char buf[1024];
-    char acClientRequest[1024] = {0};
-    int bytes;
+    int server;
     char *hostname, *portnum;
     if ( count != 3 )
     {
-        printf("usage: %s <hostname> <portnum>\n", strings[0]);
+    	printf("syntax : ssl_client <host> <port>\n");
+    	printf("sample : ssl_client 127.0.0.1 1234\n");
         exit(0);
     }
     SSL_library_init();
@@ -91,24 +112,33 @@ int main(int count, char *strings[])
         ERR_print_errors_fp(stderr);
     else
     {
-        char acUsername[16] = {0};
-        char acPassword[16] = {0};
-        const char *cpRequestMessage = "<Body>\
-                               <UserName>%s<UserName>\
-                 <Password>%s<Password>\
-                 <\Body>";
-        printf("Enter the User Name : ");
-        scanf("%s",acUsername);
-        printf("\n\nEnter the Password : ");
-        scanf("%s",acPassword);
-        sprintf(acClientRequest, cpRequestMessage, acUsername,acPassword);   /* construct reply */
-        printf("\n\nConnected with %s encryption\n", SSL_get_cipher(ssl));
+        printf("Connected with %s encryption\n", SSL_get_cipher(ssl));
         ShowCerts(ssl);        /* get any certs */
-        SSL_write(ssl,acClientRequest, strlen(acClientRequest));   /* encrypt & send message */
-        bytes = SSL_read(ssl, buf, sizeof(buf)); /* get reply & decrypt */
-        buf[bytes] = 0;
-        printf("Received: \"%s\"\n", buf);
-        SSL_free(ssl);        /* release connection state */
+        
+    	pthread_t th;
+		if(pthread_create(&th, NULL, t_func, (void *)&ssl) < 0)
+	    {
+	        perror("thread create error : ");
+	        return -1;
+	    }
+		pthread_detach(th);
+				
+		while (1)
+		{
+			char buf[BUFSIZE];
+			
+	        scanf("%s", buf);
+			if (strcmp(buf, "quit") == 0) break;
+			
+	        int sent = SSL_write(ssl, buf, strlen(buf));   /* encrypt & send message */
+	        if (sent <= 0)
+	        {
+        		SSL_get_error(ssl, sent);
+				break;
+			}
+		}
+		
+    	SSL_free(ssl);        /* release connection state */
     }
     close(server);         /* close socket */
     SSL_CTX_free(ctx);        /* release context */
--- ssl_server.cpp
+++ ssl_server.cpp
@@ -9,8 +9,124 @@
 #include <resolv.h>
 #include "openssl/ssl.h"
 #include "openssl/err.h"
+#include <pthread.h>
+#include <list>
 #define FAIL    -1
+
+const int BUFSIZE = 1024;
+
+using namespace std;
+
 // Create the SSL socket and intialize the socket address structure
+
+struct Info
+{
+	SSL *ssl;
+	int num;
+	Info()
+	{
+	}
+	Info(SSL *ssl, int num)
+	{
+		this->ssl = ssl;
+		this->num = num;
+	}
+};
+
+struct Client
+{
+	pthread_t th;
+	Info info;
+	Client(pthread_t th, SSL *ssl, int num)
+	{
+		this->th = th;
+		this->info.ssl = ssl;
+		this->info.num = num;
+	}
+};
+
+pthread_mutex_t mutex_lock;
+bool broadcast_mode = false;
+list<Client> ssl_client;
+
+void* t_func(void *data)
+{
+	Info info = (*(Info *) data);
+	SSL *ssl = info.ssl;
+	int num = info.num;
+    int sd;
+    
+    if ( SSL_accept(ssl) == FAIL )     /* do SSL-protocol accept */
+        ERR_print_errors_fp(stderr);
+    else
+    {
+    	while (1)
+    	{
+    		char buf[BUFSIZE];
+    		
+	        int received = SSL_read(ssl, buf, sizeof(buf)); /* get request */
+	        if (received <= 0) 
+	        {
+	        	SSL_get_error(ssl, received);
+	        	break;
+			}
+	        buf[received] = '\0';
+	        printf("%s\n", buf);
+	        
+	        int sent = SSL_write(ssl, buf, strlen(buf)); /* send reply */
+	        if (sent == 0)
+	        {
+        		SSL_get_error(ssl, received);
+        		break;
+			}
+			if (broadcast_mode)
+			{
+				pthread_mutex_lock(&mutex_lock);
+				for(auto it = ssl_client.begin(); it != ssl_client.end();)
+				{
+					if((*it).info.num == num)
+					{
+						it++;
+						continue;
+					}
+					int sent = SSL_write((*it).info.ssl, buf, strlen(buf));
+					if(sent <= 0)
+					{
+        				SSL_get_error(ssl, sent);
+						pthread_cancel((*it).th);
+						it = ssl_client.erase(it);
+						printf("disconnected\n");
+						break;
+					}
+					else
+					{
+						it++;
+					}
+				}
+				pthread_mutex_unlock(&mutex_lock);
+			}
+		}
+    }
+    sd = SSL_get_fd(ssl);       /* get socket connection */
+    SSL_free(ssl);         /* release SSL state */
+    close(sd);          /* close connection */
+    
+    pthread_mutex_lock(&mutex_lock);
+    for(auto it = ssl_client.begin(); it != ssl_client.end();)
+	{
+		if((*it).info.num == num)
+		{
+			it = ssl_client.erase(it);
+		}
+		else
+		{
+			it++;
+		}
+	}
+	pthread_mutex_unlock(&mutex_lock);
+	printf("disconnected\n");
+}
+
 int OpenListener(int port)
 {
     int sd;
@@ -45,10 +161,11 @@ int isRoot()
 }
 SSL_CTX* InitServerCTX(void)
 {
+	const SSL_METHOD *method;
     SSL_CTX *ctx;
     OpenSSL_add_all_algorithms();  /* load & register all cryptos, etc. */
     SSL_load_error_strings();   /* load all error messages */
-    const SSL_METHOD *method = TLSv1_2_server_method();  /* create new server-method instance */
+    method = DTLS_server_method();  /* create new server-method instance */
     ctx = SSL_CTX_new(method);   /* create new context from method */
     if ( ctx == NULL )
     {
@@ -57,7 +174,7 @@ SSL_CTX* InitServerCTX(void)
     }
     return ctx;
 }
-void LoadCertificates(SSL_CTX* ctx, char* CertFile, char* KeyFile)
+void LoadCertificates(SSL_CTX* ctx, const char* CertFile, const char* KeyFile)
 {
     /* set the local certificate from CertFile */
     if ( SSL_CTX_use_certificate_file(ctx, CertFile, SSL_FILETYPE_PEM) <= 0 )
@@ -97,63 +214,26 @@ void ShowCerts(SSL* ssl)
     else
         printf("No certificates.\n");
 }
-void Servlet(SSL* ssl) /* Serve the connection -- threadable */
-{
-    char buf[1024] = {0};
-    int sd, bytes;
-    const char* ServerResponse="<\Body>\
-                               <Name>aticleworld.com</Name>\
-                 <year>1.5</year>\
-                 <BlogType>Embedede and c\c++<\BlogType>\
-                 <Author>amlendra<Author>\
-                 <\Body>";
-    const char *cpValidMessage = "<Body>\
-                               <UserName>aticle<UserName>\
-                 <Password>123<Password>\
-                 <\Body>";
-    if ( SSL_accept(ssl) == FAIL )     /* do SSL-protocol accept */
-        ERR_print_errors_fp(stderr);
-    else
-    {
-        ShowCerts(ssl);        /* get any certificates */
-        bytes = SSL_read(ssl, buf, sizeof(buf)); /* get request */
-        buf[bytes] = '\0';
-        printf("Client msg: \"%s\"\n", buf);
-        if ( bytes > 0 )
-        {
-            if(strcmp(cpValidMessage,buf) == 0)
-            {
-                SSL_write(ssl, ServerResponse, strlen(ServerResponse)); /* send reply */
-            }
-            else
-            {
-            	static const char* http_response = "HTTP/1.1 200 ok\r\nContent-Length: 5\r\n\r\nhello";
-                SSL_write(ssl, http_response, strlen(http_response)); /* send reply */
-            }
-        }
-        else
-        {
-            ERR_print_errors_fp(stderr);
-        }
-    }
-    sd = SSL_get_fd(ssl);       /* get socket connection */
-    SSL_free(ssl);         /* release SSL state */
-    close(sd);          /* close connection */
-}
 int main(int count, char *Argc[])
 {
     SSL_CTX *ctx;
     int server;
     char *portnum;
-//Only root user have the permsion to run the server
+	//Only root user have the permsion to run the server
     if(!isRoot())
     {
         printf("This program must be run as root/sudo user!!");
         exit(0);
     }
-    if ( count != 2 )
+    if ( count == 3 && strcmp(Argc[2], "-b") == 0)
     {
-        printf("Usage: %s <portnum>\n", Argc[0]);
+    	broadcast_mode = true;
+	}
+    else if ( count != 2 )
+    {
+    	broadcast_mode = false;
+        printf("syntax : ssl_server <port> [-b]\n");
+        printf("sample : ssl_server 1234 -b\n");
         exit(0);
     }
     // Initialize the SSL library
@@ -162,16 +242,36 @@ int main(int count, char *Argc[])
     ctx = InitServerCTX();        /* initialize SSL */
     LoadCertificates(ctx, "test.com.crt", "test.com.key"); /* load certs */
     server = OpenListener(atoi(portnum));    /* create server socket */
+    
+    int num = 1;
     while (1)
     {
         struct sockaddr_in addr;
         socklen_t len = sizeof(addr);
         SSL *ssl;
         int client = accept(server, (struct sockaddr*)&addr, &len);  /* accept connection as usual */
+        if (client < 0)
+		{
+			perror("ERROR on accept");
+			break;
+		}
+		
         printf("Connection: %s:%d\n",inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
         ssl = SSL_new(ctx);              /* get new SSL state with context */
         SSL_set_fd(ssl, client);      /* set connection socket to SSL state */
-        Servlet(ssl);         /* service connection */
+        
+        pthread_t th;
+        Info info(ssl, num);
+		if(pthread_create(&th, NULL, t_func, (void *)&info) < 0)
+	    {
+	        perror("thread create error : ");
+	        return -1;
+	    }
+		pthread_detach(th);
+		
+		pthread_mutex_lock(&mutex_lock);
+		ssl_client.push_back(Client(th, ssl, num++));
+		pthread_mutex_unlock(&mutex_lock);
     }
     close(server);          /* close server socket */
     SSL_CTX_free(ctx);         /* release context */
